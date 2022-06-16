39d4715303926236aa0a091b7db1651b1f2b6850
hondayuki <yuki.honda@rakus-partners.co.jp>
2022-06-16 17:22:15 +0900

    test

--- src/test/java/com/example/repository/CommunityTestRepository.java
+++ src/test/java/com/example/repository/CommunityTestRepository.java
@@ -1,7 +1,9 @@
 package com.example.repository;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertIterableEquals;
 
+import java.util.Arrays;
 import java.util.List;
 
 import org.junit.jupiter.api.AfterAll;
@@ -11,7 +13,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
+import com.example.domain.Category;
 import com.example.domain.Community;
+import com.example.domain.Hobby;
+import com.example.domain.PostCommunity;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -35,6 +40,37 @@ public class CommunityTestRepository {
 	@Test
 	public void findByHobbyId() {
 		List<Community> communityList = communityRepository.findByHobby(5);
+		Community community = new Community();
+		community.setComId(1);
+		community.setName("サンプル1");
+		
+		Category category = new Category();
+		category.setCategoryId(1);
+		category.setName("J-POP");
+		
+		Hobby hobby = new Hobby();
+		hobby.setHobbyId(5);
+		hobby.setName("音楽");
+		
+		category.setHobby(hobby);
+		community.setCategory(category);
+		List<Community> sampleList = Arrays.asList(community);
+		
 		assertEquals("サンプル1", communityList.get(0).getName());
+		assertIterableEquals(sampleList, communityList);
+	}
+	
+	@Test
+	public void insertTest() {
+		PostCommunity postCommunity = new PostCommunity();
+		postCommunity.setName("サンプル4");
+		postCommunity.setCategory("sample");
+		postCommunity.setHobbyId(1);
+		communityRepository.insert(postCommunity);
+		
+		List<Community> communityList = communityRepository.findByHobby(1);
+		
+		assertEquals(2, communityList.size());
+		assertEquals("サンプル2", communityList.get(1).getName());
 	}
 }
